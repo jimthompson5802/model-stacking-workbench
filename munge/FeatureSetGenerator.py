5e9230c2c9c92ad086f0ac1fb0af2763edd9910f
Jim Thompson <jimthompson5802@aol.com>
2018-06-01 21:58:11 -0400

    initial set up

---
+++ munge/FeatureSetGenerator.py
@@ -0,0 +1,37 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import pandas as pd
+import numpy as np
+import os
+import os.path
+import yaml
+
+
+# In[2]:
+
+CONFIG_DIR = '/Users/jim/Desktop/Kaggle/ensemble-stacking-framework'
+
+
+# In[3]:
+
+with open(os.path.join(CONFIG_DIR,'config.yml')) as f:
+    config = yaml.load(f.read())
+
+
+# In[4]:
+
+config
+
+
+# In[ ]:
+config
+
+
+
+# In[ ]:
+
+
+
