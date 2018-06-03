9d9764be7c658189c1e0fbd96541615fa19c6ecd
Jim Thompson <jimthompson5802@aol.com>
2018-06-03 10:48:57 -0400

    added function to generate features from hold out fold

--- models/L0RF1/train_model.py
+++ models/L0RF1/train_model.py
@@ -7,6 +7,7 @@ import pickle
 import os.path
 import pandas as pd
 import numpy as np
+import functools
 #%%
 #
 # get parameters 
@@ -20,7 +21,7 @@ train_ds = 'train.csv'
 test_df = 'test.csv'
 feature_set = 'L0FS02'
 model_id = 'L0RF1'
-model_params = dict(n_estimators=20,n_jobs=4)
+model_params = dict(n_estimators=20,n_jobs=-1)
 
 #%%
 #
@@ -31,16 +32,20 @@ with open(os.path.join(CONFIG['ROOT_DIR'],'data/fold_specification.pkl'),'rb') a
     
     
 #%%
+#
+# generate features for next level
+#
+
 # retrieve training data
 train_df = pd.read_csv(os.path.join(CONFIG['ROOT_DIR'],'data',feature_set,train_ds))
 
 predictors = sorted(list(set(train_df) - set(CONFIG['ID_VAR']) - set([CONFIG['TARGET_VAR']])))
 
 
-X1 = train_df.loc[:,predictors]
-y1 = train_df[CONFIG['TARGET_VAR']]
-
-
+#
+# create features for next level using the hold out set
+#
+next_level = []
 i = 0
 for fold in k_folds:
     i += 1
@@ -57,18 +62,29 @@ for fold in k_folds:
     #generate feature for next level
     # get indices for hold out set
     holdout_idx = fold[1]
+    
+    # set up predictors and target for hold out set
     X_holdout = train_df.iloc[holdout_idx,:]
     id_holdout = X_holdout.loc[:,CONFIG['ID_VAR']]
     X_holdout = X_holdout.loc[:,predictors]
     y_holdout = train_df[CONFIG['TARGET_VAR']].iloc[holdout_idx]
 
-    #
+    # geneate features for next level
     y_hat = pd.DataFrame(model.predict_proba(X_holdout),index=id_holdout.index)
+    y_hat.columns = [model_id+'_'+str(col) for col in y_hat.columns]
+    y_hat = id_holdout.join(y_holdout).join(y_hat)
+    
+    next_level.append(y_hat)
     
 
-
 #%%
-model = MODEL(**model_params)
-print(model.get_params())
+#
+# combine the generated features into single dataframe & save to disk
+#
+pd.concat(next_level).sort_values(CONFIG['ID_VAR'])\
+    .to_csv(os.path.join(CONFIG['ROOT_DIR'],'models',model_id,model_id+'_features.csv'),
+            index=False)
+
+
 
 
