8c152dbf4be44419e8743354ad4aed3875aa5073
Jim Thompson <jimthompson5802@aol.com>
2018-06-03 08:34:57 -0400

    added Feature Set 2 generation

--- munge/CreateLevel0Features.py
+++ munge/CreateLevel0Features.py
@@ -4,6 +4,7 @@ from framework.model_stacking import FeatureGenerator
 import yaml
 import os.path 
 import pandas as pd
+import numpy as np
 
 
 #%%
@@ -19,18 +20,47 @@ print('root dir: ',CONFIG['ROOT_DIR'])
 #
 # feature set 1
 #
-fs1 = FeatureGenerator(CONFIG['ROOT_DIR'],'raw','L0FS01')
+fs = FeatureGenerator(CONFIG['ROOT_DIR'],'raw','L0FS01')
 
 # get raw data
-fs1.getRawData()
+fs.getRawData()
 
-new_train = fs1.raw_train_features_df
+new_train = fs.raw_train_features_df
 new_train.fillna(-999,inplace=True)
 
-new_test = fs1.raw_test_features_df
+new_test = fs.raw_test_features_df
 new_test.fillna(-999,inplace=True)
 
 #%%
-fs1.saveFeatureSet(new_train, new_test)
+fs.saveFeatureSet(new_train, new_test)
 
+
+#%%
+#
+# feature set 2
+#
+fs = FeatureGenerator(CONFIG['ROOT_DIR'],'raw','L0FS02')
+
+# get raw data
+fs.getRawData()
+
+new_train = fs.raw_train_features_df
+
+#%%
+# find only numberic attributes
+numeric_predictors = [x for x in new_train.columns if new_train[x].dtype != 'O']
+numeric_predictors
+#%%
+new_train = new_train.loc[:,numeric_predictors]
+new_train.fillna(-999,inplace=True)
+new_train.shape
+
+
+#%%
+new_test = fs.raw_test_features_df.loc[:,numeric_predictors]
+new_test.fillna(-999,inplace=True)
+new_test.shape
+
+#%%
+fs.saveFeatureSet(new_train, new_test)
 
