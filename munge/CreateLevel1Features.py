60c045d5b54ce9c977da7375b0df8cf3fd0f5812
Jim Thompson <jimthompson5802@aol.com>
2018-06-03 23:01:15 -0400

    code check-in

--- munge/CreateLevel1Features.py
+++ munge/CreateLevel1Features.py
@@ -1,3 +1,4 @@
+#%%
 from framework.model_stacking import FeatureGenerator
 
 #%%
@@ -25,11 +26,17 @@ class FeatureGeneratorNextLevel(FeatureGenerator):
     def getRawData(self):
         features = []
         for model_id in self.in_dir:
-            df = pd.read_csv(os.path.join(self.CONFIG['ROOT_DIR'],
+            train_df = pd.read_csv(os.path.join(self.CONFIG['ROOT_DIR'],
                                           'models',
                                           model_id,
-                                          model_id+'_features.csv'))
-            features.append(df)
+                                          model_id+'_train_features.csv'))
+
+            test_df = pd.read_csv(os.path.join(self.CONFIG['ROOT_DIR'],
+                                          'models',
+                                          model_id,
+                                          model_id+'_test_features.csv'))
+             
+            features.append((train_df,test_df))
             
         return features
     
@@ -38,13 +45,29 @@ class FeatureGeneratorNextLevel(FeatureGenerator):
 fs = FeatureGeneratorNextLevel(in_dir=['L0RF1','L0NN1'],
                                out_dir='L1FS01')
 
-#%%
-print(fs.in_dir)
-print(fs.out_dir)
-print(fs.CONFIG)
-
 
 #%%
-df = fs.getRawData()
+#
+# retrieve candidate train and test data sets from prior level
+#
+# this is a list of tuples.  Each tuple structure: (train_df, test_df)
+features = fs.getRawData()
 
 #%%
+#
+# Assemble train and test data sets for next Level
+#
+
+# assemble train data set
+found_first_df = False
+for t in features:
+    if not found_first_df:
+        train_df = t[0].iloc[:,:2].join(t[0].iloc[:,3])
+        test_df = t[1].iloc[:,:1].join(t[1].iloc[:,2])
+        found_first_df = True
+    else:
+        train_df = train_df.join(t[0].iloc[:,3])
+        test_df = test_df.join(t[1].iloc[:,2])
+        
+#%%
+fs.saveFeatureSet(train_df,test_df)
