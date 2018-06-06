f067e7abaedb66c9cd92706e8e33c9c1fd4410f4
Jim Thompson <jimthompson5802@aol.com>
2018-06-05 22:40:34 -0400

    added Extra Tree Classifiers model

---
+++ models/L0XTC1/train_model.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#%%
+from framework.model_stacking import ModelTrainer, ModelPerformanceTracker
+from sklearn.ensemble import ExtraTreesClassifier as ThisModel
+
+#%%
+#
+# Set up model for training
+#
+this_model = ModelTrainer(
+        ModelClass=ThisModel,  #Model algorithm
+        model_params=dict(n_estimators=200,n_jobs=-1), #hyper-parameters
+        model_id='L0XTC1',   # Model Identifier
+        feature_set='L0FS02'  # feature set to use
+        )
+
+model_tracker = ModelPerformanceTracker(model_trainer=this_model)
+#%%
+#
+# clear out old results
+#
+this_model.cleanPriorResults()
+
+#%%
+#
+# create features for the next level
+#
+this_model.createFeaturesForNextLevel()
+
+#%%
+#
+# train model on all the data
+#
+this_model.trainModel()
+
+#%%
+#
+# create Kaggle submission
+#
+this_model.createKaggleSubmission()
+
+#%%
+#
+# record model performance metrics
+#
+model_tracker.recordModelPerformance()
+
+#%%
