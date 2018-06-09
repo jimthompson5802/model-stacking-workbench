fcd4db34c5c40d73bfb524d551fd1f39a85b924d
Jim Thompson <jimthompson5802@aol.com>
2018-06-09 11:53:02 -0400

    initial check-in

---
+++ models/L0LOG1/train_model.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#%%
+from framework.model_stacking import ModelTrainer, ModelPerformanceTracker
+from sklearn.linear_model import LogisticRegression as ThisModel
+
+#%%
+#
+# Set up model for training
+#
+this_model = ModelTrainer(
+        ModelClass=ThisModel,  #Model algorithm
+        model_params=dict(penalty='l1',C=10.0,random_state=13), #hyper-parameters
+        model_id='L0LOG1',   # Model Identifier
+        feature_set='KFS04'  # feature set to use
+        )
+
+model_tracker = ModelPerformanceTracker(model_trainer=this_model)
+#%%
+#
+# clear out old results
+#
+this_model.cleanPriorResults()
+
+#%%
+#
+# train model on all the data
+#
+this_model.trainModel()
+
+#%%
+# create Test predictions
+this_model.createTestPredictions()
+
+#%%
+#
+# create Kaggle submission
+#
+this_model.createKaggleSubmission()
+
+#%%
+#
+# record model performance metrics
+#
+model_tracker.recordModelPerformance()
+
+#%%
